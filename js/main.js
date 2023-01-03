79c9fb17e34b5c9105f514cbf1f02462d6871961
genyaSho <121571971+genyaSho@users.noreply.github.com>
2023-01-03 17:28:28 +0200

    new

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function(){
+    $('.top__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        fade: true,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                dots: false
+              }
+            },
+          ]
+    });
+
+    $('.reviews__slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+    })
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active')
+    });
+
+});
